24b3acb5ce6f1b49f080f8b2de77e10285f0c5ab
KittySmile <15770838310@163.com>
2022-10-11 21:03:00 +0800

    first commit

---
+++ src/main/java/com/run/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.run.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Date: 2022/09/18/18:40
+ * @Description:
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping()
+    public String test01(){
+        return "Hello test01";
+    }
+
+}
